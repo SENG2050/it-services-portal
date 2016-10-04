86871703bda2b62fbf8788ebe452d9b32b809266
Mitchell Davis <mitchell.peter.davis@gmail.com>
2016-10-04 15:47:22 +1100

    Added JSON library. Updated routes to not use wildcards for search or suggest routes.

--- src/KB_SuggestController.java
+++ src/KB_SuggestController.java
@@ -3,6 +3,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonArrayBuilder;
 
 /**
  * Searches all KB articles for the given term and returns them in a JSON structure
