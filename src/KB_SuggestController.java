359f5c064c86f196468b156cd2f3dd00fec2b7d8
Mitchell Davis <mitchell.peter.davis@gmail.com>
2016-10-04 15:39:04 +1100

    Added suggest controller.

---
+++ src/KB_SuggestController.java
@@ -0,0 +1,14 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Searches all KB articles for the given term and returns them in a JSON structure
+ */
+public class KB_SuggestController extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
