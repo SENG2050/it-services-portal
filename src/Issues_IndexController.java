19e0c6e049c71824425be8030ce8c10992e2d8bf
Mitchell Davis <mitchell.peter.davis@gmail.com>
2016-10-07 21:31:19 +1100

    Added proof-of-concept with printing issues in JSP.

--- src/Issues_IndexController.java
+++ src/Issues_IndexController.java
@@ -3,12 +3,21 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * Shows the admin a list of all issues
  */
 public class Issues_IndexController extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        ArrayList<Issue> issues = new ArrayList<Issue>();
+
+        issues.add(new Issue());
+        issues.add(new Issue());
+        issues.add(new Issue());
+
+        request.setAttribute("issues", issues);
+
         request.getRequestDispatcher("/WEB-INF/jsp/issues/index.jsp").forward(request, response);
     }
 }
--- web/WEB-INF/jsp/issues/index.jsp
+++ web/WEB-INF/jsp/issues/index.jsp
@@ -1,11 +1,19 @@
 <%@page contentType="text/html" pageEncoding="UTF-8" %>
 <%@taglib prefix="t" tagdir="/WEB-INF/tags" %>
+<%@taglib prefix="c" uri="http://java.sun.com/jstl/core_rt" %>
 
 <t:page>
     <jsp:attribute name="title">
-        Issues Index Page
+        Issues
+    </jsp:attribute>
+    <jsp:attribute name="breadcrumbs">
+        <ol class="breadcrumb pull-right">
+            <li class="active">Issues</li>
+        </ol>
     </jsp:attribute>
     <jsp:body>
-        <p>Issues Index Page</p>
+        <c:forEach items="${issues}" var="item">
+            <p>Issue</p>
+        </c:forEach>
     </jsp:body>
 </t:page>
